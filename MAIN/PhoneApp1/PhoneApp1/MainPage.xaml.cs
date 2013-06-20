504d9b97de032c1d6f1e8e56f60b3b2361959779
thomas <thoto33tennis@hotmail.fr>
2013-06-20 16:11:25 +0200

    nouvelle structure du code plus facile

--- MAIN/PhoneApp1/PhoneApp1/MainPage.xaml.cs
+++ MAIN/PhoneApp1/PhoneApp1/MainPage.xaml.cs
@@ -15,23 +15,38 @@ namespace PhoneApp1
     public partial class MainPage : PhoneApplicationPage
     {
         // Constructeur
-        public MainPage()
-        {
-         InitializeComponent();
-        }
+            public MainPage()
+            {
+                InitializeComponent();
+ 
+                ApplicationBar = new ApplicationBar();
+ 
+                ApplicationBar.Mode = ApplicationBarMode.Default;
+                ApplicationBar.Opacity = 1.0; 
+                ApplicationBar.IsVisible = true;
+                ApplicationBar.IsMenuEnabled = true;
+ 
+                ApplicationBarIconButton button1 = new ApplicationBarIconButton();
+                button1.IconUri = new Uri("/Assets/Tiles/check.png", UriKind.Relative);
+                button1.Text = "sign in";
+                ApplicationBar.Buttons.Add(button1);
+                button1.Click += new EventHandler(button1_Click);
 
-        private void Go_Click(object sender, RoutedEventArgs e)
-        {
-            Button bouton = (Button)sender;
-            bouton.Content = "OK !";
-            aff_log.Text = Login.Text;
-            aff_mdp.Text = Mdp.Password;          
-        }
-            
-        private void Intra_Click_1(object sender, RoutedEventArgs e)
-        {
-            Button btn = (Button)sender;
-            btn.Content = "Let's go !";
-		}
+                ApplicationBarIconButton button2 = new ApplicationBarIconButton();
+                button2.IconUri = new Uri("/Assets/Tiles/feature.search.png", UriKind.Relative);
+                button2.Text = "intra";
+                ApplicationBar.Buttons.Add(button2);
+                button2.Click += new EventHandler(button2_Click);
+           }
+ 
+            private void button1_Click(object sender, EventArgs e)
+             {
+                aff_log.Text = Login.Text;
+                aff_mdp.Text = Mdp.Password;   
+             }
+            private void button2_Click(object sender, EventArgs e)
+             {
+                NavigationService.Navigate(new Uri("/Page1.xaml", UriKind.Relative));
+             }
     }
 }
